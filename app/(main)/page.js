4bd8e895edff2c310867641e1d75beff2fce9ab1
sanjit-kushvanshi <tiwariprachy@gmail.com>
2026-08-04 11:08:24 +0545

    Add dynamic export to page.js

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { createClient } from "@/lib/supabase/server";
 import StoriesRow from "@/components/StoriesRow";
 import PostCard from "@/components/PostCard";
