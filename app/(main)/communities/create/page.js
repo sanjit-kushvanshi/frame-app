77d9837f54ff303c21168bebd76a7ab8bb80af3c
sanjit-kushvanshi <tiwariprachy@gmail.com>
2026-08-28 19:42:15 +0545

    Update page.js

--- app/(main)/communities/create/page.js
+++ app/(main)/communities/create/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import { createClient } from "@/lib/supabase/client";
 
 export default function CreateCommunityPage() {
   const router = useRouter();
