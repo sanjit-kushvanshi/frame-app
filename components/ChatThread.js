99cfec784f66087ecdf6e38dd51d796f826109ae
sanjit-kushvanshi <tiwariprachy@gmail.com>
2026-09-04 12:52:45 +0545

    Fix formatting of viewOnceOpened variable assignment

--- components/ChatThread.js
+++ components/ChatThread.js
@@ -737,7 +737,7 @@ if (!error && data) {
           const isMe = m.sender_id === currentUserId;
           const sender = isGroup && !isMe ? profileById(m.sender_id) : null;
           const viewOnceLocked = m.view_once && !isMe && !myViewedOnceIds.has(m.id) && revealedId !== m.id;
-          const viewOnceOpened = m.view_once && !isMe && myViewedOnceIds.has(m.id) && revealedId !== m.id;
+const viewOnceOpened = m.view_once && !isMe && myViewedOnceIds.has(m.id) && revealedId !== m.id;
 
           if (m.deleted) {
             return (
